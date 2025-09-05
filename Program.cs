cd01f5a1fcc32e75eca498a72498516fc2d7a5c2
Berdila17 <l.berdila.inf24@stud.bbbaden.ch>
2025-09-05 12:17:45 +0200

    Program.cs

--- Program.cs
+++ Program.cs
@@ -4,17 +4,19 @@ namespace RPG
     {
         public static void Main(string[] args)
         {
-            // Magier erstellen 
+            // Spieler erstellen und testen
             var merlin = new Spieler("Merlin", Spielerklasse.Magier, 60);
             merlin.Vorstellen();
 
             // Spieler nimmt Schaden
             merlin.NimmSchaden(15);
-            Console.Write("Nach Schaden: "); merlin.Vorstellen();
+            Console.Write("Nach Schaden: ");
+            merlin.Vorstellen();
 
             // Spieler heilt sich wieder
             merlin.Heilen(10);
-            Console.Write("Nach Heilen:  "); merlin.Vorstellen();
+            Console.Write("Nach Heilen:  ");
+            merlin.Vorstellen();
 
             // Andere Klassen testen
             var arthur = new Spieler("Arthur", Spielerklasse.Krieger, 100);
@@ -23,11 +25,18 @@ namespace RPG
             arthur.Vorstellen();
             sly.Vorstellen();
 
-            // IstLebendig() testen
             Console.WriteLine($"Lebt Merlin? {merlin.IstLebendig()}");
+            Console.WriteLine();
 
-            Console.ReadLine(); 
+            // Gegner erstellen und Angreifen testen
+            var goblin = new Gegner("Goblin", 40, 8);
+            goblin.Vorstellen();
+
+            goblin.Angreifen(merlin);  // Gegner verursacht Schaden am Spieler
+            Console.Write("Nach Goblin-Angriff: ");
+            merlin.Vorstellen();
+
+            Console.ReadLine();
         }
     }
 }
-
