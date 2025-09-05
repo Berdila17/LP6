e94a703e4c13daecdf5e394d437fba06974a5c54
Berdila17 <l.berdila.inf24@stud.bbbaden.ch>
2025-09-05 12:15:26 +0200

    waffe.cs

--- waffe.cs
+++ waffe.cs
@@ -1,20 +1,20 @@
-namespace TextRPG
+namespace RPG
 {
-    // Klasse beschreibt eine Waffe 
+    // Klasse
     public class Waffe
     {
         // Eigenschaften 
         public string Name { get; }
         public int Schaden { get; }
 
-        //Waffe wird beim Erstellen mit Name und Schaden initialisiert
+        
         public Waffe(string name, int schaden)
         {
             Name = name;
             Schaden = schaden;
         }
 
-        // Überschreiben von ToString damit beim Ausgeben der Waffenname angezeigt wird (Mit ChatGBT gemacht er hat mir diesen Tipp gegeben)
+        // Anzeigen in der Konsole
         public override string ToString() => $"{Name} (Schaden {Schaden})";
     }
 }
